63de7bb8b223771b049d2397239a0a40fc0806b9
Darman <n.darman05@gmail.com>
2023-07-31 11:01:49 +0600

    Импортировать css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("TEST");
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import ToDoList from './components/todo-list';
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 },
+  ]
+
+  return (
+    <div>
+    <AppHeader />
+    <SearchPanel />
+    <ToDoList todos={todoData}/>
+  </div>
+  )
+}
+
+root.render(<App />)
