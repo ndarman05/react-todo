969972b4965dbd1daa98d8bc0c55c2ef1a59d014
Darman <n.darman05@gmail.com>
2023-08-01 13:51:54 +0600

    Структура react проекта часть 2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ToDoList from './components/todo-list';
+import App from './components/app';
+
+import './index.css';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ]
-
-  return (
-    <div>
-    <AppHeader />
-    <SearchPanel />
-    <ToDoList todos={todoData}/>
-  </div>
-  )
-}
 
 root.render(<App />)
